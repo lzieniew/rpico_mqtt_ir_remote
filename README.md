48b3a122e6a94439e62a83467fe9625c5346c363
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-02-17 16:58:53 +0100

    MQTT works, IR not yet implemented

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rpico_mqtt_ir_remote
 Script for raspberry pi pico to send IR signals in reaction to MQTT messages
+
+You will need to put umqtt library on rpi pico. You can do it by cloning https://github.com/micropython/micropython-lib and putting file micropython/umqtt.simple/umqtt/simple.py on the pico. It can be done with command
+`sudo ampy --port /dev/ttyACM0 put micropython-lib/micropython/umqtt.simple/umqtt/simple.py /lib/umqtt/simple.py`
+I order to do this there has to be /lib/umqtt directory on raspberry pico. You can create it with rshell:
+- `sudo rshell -p /dev/ttyACM0`
+- then go `cd /pyboard`
+- `mkdir lib/umqtt`
+
+upload to rpico with 
+`sudo ampy -p /dev/ttyACM0 put main.py`
+
---
+++ main.py
@@ -0,0 +1,54 @@
+import machine
+import utime
+import network
+from umqtt.simple import MQTTClient
+
+# WiFi details
+ssid = "your_ssid"
+password = "your_password"
+
+# MQTT details
+mqtt_server = "ejaj.local"
+# If you're using a specific port for MQTT (default is 1883), include it here
+mqtt_port = 1883
+
+
+# Connect to WiFi
+def connect_to_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    # Wait for connection
+    while not wlan.isconnected():
+        pass
+
+    print("Connected to WiFi")
+
+
+# Callback function to handle received messages
+def on_message(topic, msg):
+    print("Received message:", topic, msg)
+    pin_15.toggle()
+
+
+# Set up pin 15 for MQTT message toggle
+pin_15 = machine.Pin(15, machine.Pin.OUT)
+
+# Connect to WiFi
+connect_to_wifi()
+
+# Set up MQTT client
+client = MQTTClient("pico_w_client", mqtt_server, port=mqtt_port)
+client.set_callback(on_message)
+client.connect()
+client.subscribe("test/topic")
+
+print("Connected to MQTT broker, subscribed to topic")
+
+# Main loop
+try:
+    while True:
+        client.wait_msg()  # Wait for messages, this is a blocking call
+except KeyboardInterrupt:
+    client.disconnect()
